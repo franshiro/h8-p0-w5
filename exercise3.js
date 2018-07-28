7ca3b462c2ab96536ce80ce1cce10b96e708e37f
franshiro <franshiro@gmail.com>
2018-07-29 01:58:46 +0700

    mencari nilai tertinggi pada obj

---
+++ exercise3.js
@@ -0,0 +1,76 @@
+function highestScore (students) {
+    // Code disini
+    var objScore = {}
+    for(var i = 0; i<students.length; i++){
+      if(typeof objScore[students[i].class] === 'undefined'){
+        objScore[ students[i].class ] = {
+          name : students[i].name,
+          score : students[i].score
+        }
+      }
+    }
+    return objScore
+  }
+  
+  // TEST CASE
+  console.log(highestScore([
+    {
+      name: 'Dimitri',
+      score: 90,
+      class: 'foxes'
+    },
+    {
+      name: 'Alexei',
+      score: 85,
+      class: 'wolves'
+    },
+    {
+      name: 'Sergei',
+      score: 74,
+      class: 'foxes'
+    },
+    {
+      name: 'Anastasia',
+      score: 78,
+      class: 'wolves'
+    }
+  ]));
+  
+  // {
+  //   foxes: { name: 'Dimitri', score: 90 },
+  //   wolves: { name: 'Alexei', score: 85 }
+  // }
+  console.log('------------------------------------')
+  
+  console.log(highestScore([
+    {
+      name: 'Alexander',
+      score: 100,
+      class: 'foxes'
+    },
+    {
+      name: 'Alisa',
+      score: 76,
+      class: 'wolves'
+    },
+    {
+      name: 'Vladimir',
+      score: 92,
+      class: 'foxes'
+    },
+    {
+      name: 'Albert',
+      score: 71,
+      class: 'wolves'
+    },
+    {
+      name: 'Viktor',
+      score: 80,
+      class: 'tigers'
+    }
+  ]));
+  // {
+  //   foxes: { name: 'Alexander', score: 100 },
+  //   wolves: { name: 'Alisa', score: 76 },
+  //   tigers: { name: 'Viktor', score: 80 }
+  // }
