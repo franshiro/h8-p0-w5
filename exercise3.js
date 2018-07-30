6f569514bea6b0cdf0eddfe1bb65aec76dc82ab3
franshiro <franshiro@gmail.com>
2018-07-30 11:53:53 +0700

    perbaiki coding highest score

--- exercise3.js
+++ exercise3.js
@@ -8,17 +8,17 @@ function highestScore (students) {
           score : students[i].score
         }
       }
+      if(objScore[ students[i].class ].score < students[i].score){
+        objScore[ students[i].class ].name = students[i].name
+        objScore[ students[i].class ].score = students[i].score
+      }
     }
     return objScore
   }
   
   // TEST CASE
   console.log(highestScore([
-    {
-      name: 'Dimitri',
-      score: 90,
-      class: 'foxes'
-    },
+    
     {
       name: 'Alexei',
       score: 85,
@@ -26,12 +26,17 @@ function highestScore (students) {
     },
     {
       name: 'Sergei',
-      score: 74,
+      score: 91,
+      class: 'foxes'
+    },
+    {
+      name: 'Dimitri',
+      score: 90,
       class: 'foxes'
     },
     {
       name: 'Anastasia',
-      score: 78,
+      score: 90,
       class: 'wolves'
     }
   ]));
@@ -41,36 +46,3 @@ function highestScore (students) {
   //   wolves: { name: 'Alexei', score: 85 }
   // }
   console.log('------------------------------------')
-  
-  console.log(highestScore([
-    {
-      name: 'Alexander',
-      score: 100,
-      class: 'foxes'
-    },
-    {
-      name: 'Alisa',
-      score: 76,
-      class: 'wolves'
-    },
-    {
-      name: 'Vladimir',
-      score: 92,
-      class: 'foxes'
-    },
-    {
-      name: 'Albert',
-      score: 71,
-      class: 'wolves'
-    },
-    {
-      name: 'Viktor',
-      score: 80,
-      class: 'tigers'
-    }
-  ]));
-  // {
-  //   foxes: { name: 'Alexander', score: 100 },
-  //   wolves: { name: 'Alisa', score: 76 },
-  //   tigers: { name: 'Viktor', score: 80 }
-  // }
